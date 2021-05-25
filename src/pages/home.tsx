123574a35f18bebc00f2f4431ec0dcbcacc93cb6
Pavel Srom <srompavel98@gmail.com>
2021-05-25 17:54:51 +0200

    feat(homepage): layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,99 @@
+import { Container } from '@material-ui/core'
+import clsx from 'clsx'
 import { Link } from 'react-router-dom'
-import { Button } from '../styleguide'
+import { Button, Text } from '../styleguide'
+
+const featureTexts = [
+  'Share your projects and get feedback',
+  'Ask questions and get help',
+  'Get inspiration from other projects',
+  'Get an overview over your projects',
+]
 
 export const Home: React.FC = () => {
   return (
-    <div>
-      <p>Home</p>
-      <Button component={Link} to="/register">
-        Go to register
-      </Button>
-      <Button component={Link} to="/login">
-        Go to login
-      </Button>
-    </div>
+    <>
+      <header className="h-16 bg-theme-secondary text-white text-center p-4">
+        <Text variant="h2">
+          New features coming up soon, register and stay tuned!
+        </Text>
+      </header>
+
+      <section className="bg-white">
+        <nav className="px-12 space-x-6 pt-8 flex justify-end">
+          <Button variant="text" component={Link} to="/">
+            Home
+          </Button>
+          <Button variant="text" component="a" to="#features">
+            Features
+          </Button>
+          <Button variant="text" component={Link} to="/register">
+            Register
+          </Button>
+          <Button variant="text" component={Link} to="/login">
+            Login
+          </Button>
+        </nav>
+
+        <div className="text-center mt-24 space-y-4">
+          <Text variant="h2" className="text-3xl">
+            School made easy
+          </Text>
+          <Text variant="h1" className="text-5xl">
+            A safe community
+            <br />
+            for students and teachers
+          </Text>
+          <Text variant="body" className="text-2xl">
+            Our platform creates a shared space where students
+            <br /> can share ideas, projects and much more
+          </Text>
+        </div>
+
+        <img
+          src="/images/home_banner.svg"
+          alt="home_banner.svg"
+          className="max-w-full h-auto mx-auto mt-16"
+        />
+      </section>
+
+      <Container
+        component="section"
+        maxWidth="lg"
+        className="py-16 flex flex-col"
+        id="features"
+      >
+        <Text variant="h1" className="text-center text-5xl mb-16">
+          Features
+        </Text>
+
+        {featureTexts.map((text, index) => (
+          <div
+            key={index}
+            className={clsx('w-1/2 flex justify-center', {
+              'self-end': index % 2 === 1,
+            })}
+          >
+            <div>
+              <Text className="text-2xl mb-6">{text}</Text>
+              <img
+                src={`/images/home${index + 1}.svg`}
+                alt={`home${index + 1}.svg`}
+                className="max-w-xs h-auto"
+              />
+            </div>
+          </div>
+        ))}
+      </Container>
+      <img
+        src="/images/home_bottom.svg"
+        alt="home_bottom.svg"
+        className="max-w-full h-auto mx-auto mt-16"
+      />
+
+      <footer className="h-8 flex justify-center items-center bg-gray-700">
+        <Text className="text-white">Copyright</Text>
+      </footer>
+    </>
   )
 }
